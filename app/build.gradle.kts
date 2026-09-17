713af499364077c8c5a7608c4cac1ca9f2251044
proboom481-dot <proboom481@gmail.com>
2026-09-17 21:13:14 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 }
 
 dependencies {
+implementation("androidx.compose.material:material-icons-extended")
     implementation(platform("androidx.compose:compose-bom:2024.09.03"))
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.compose.ui:ui")
